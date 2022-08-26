bbb296b511923e1916f66b6c0356f6b6c043f087
Ben van der Meer <benvdmr@gmail.com>
2022-08-26 14:06:33 +0200

    Changed every Date() reference from the BankAccount files, because parsing and/or setting it in acceptable format with Date() gave to many problems. Everything is LocalDateTime now, and works. Maybe TODO: create separate service class for getting LocalDateTime.now()

--- src/test/java/com/example/kamervankrypto/repository/BankAccountRepositoryTest.java
+++ src/test/java/com/example/kamervankrypto/repository/BankAccountRepositoryTest.java
@@ -2,18 +2,18 @@ package com.example.kamervankrypto.repository;
 
 import com.example.kamervankrypto.model.BankAccount;
 import com.example.kamervankrypto.model.Trader;
-import org.mockito.Mock;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.mockito.Mockito;
-import org.junit.jupiter.api.*;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
-import java.util.List;
 
-import static com.fasterxml.jackson.databind.type.LogicalType.DateTime;
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class BankAccountRepositoryTest {
@@ -26,26 +26,28 @@ public class BankAccountRepositoryTest {
     void setup() throws ParseException {
         mockBankAccountDAO = Mockito.mock(BankAccountDAO.class);
 
-        List<Trader> mockTraderList = new ArrayList<>();
+        //List<Trader> mockTraderList = new ArrayList<>();
         String sBirthDate = "11/01/1990";
         Date birthDate = new SimpleDateFormat("dd/MM/yyyy").parse(sBirthDate);
 
-        mockTrader = new Trader(1,"a@b.com", "Password", "Piet1", "de", "Weetikniet1", 123456789, birthDate, "Straat", "5a", "1234ab", "Stadje", true );
-        BankAccount mockBankAccount = new BankAccount(12345.25, DateTime.toString(), "NL23KVKB356985759" );
+        LocalDateTime dateNow = LocalDateTime.now();
+        DateTimeFormatter DATEFORMAT = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+        mockTrader = new Trader(1, "a@b.com", "Password", "Piet1", "de", "Weetikniet1", 123456789, birthDate, "Straat", "5a", "1234ab", "Stadje", true);
+        BankAccount mockBankAccount = new BankAccount(12345.25, LocalDateTime.parse(dateNow.format(DATEFORMAT), DATEFORMAT), "NL23KVKB356985759");
 
         Mockito.when(mockBankAccountDAO.getBankAccount(1)).thenReturn(mockBankAccount);
         bankAccountRepositoryTested = new BankAccountRepository(mockBankAccountDAO);
     }
 
     @Test
-    void test_build_of_BankAccount1(){
+    void test_build_of_BankAccount1() {
         BankAccount testBankAccount = bankAccountRepositoryTested.getBankAccount(mockTrader);
 
         System.out.println(testBankAccount.toString());
         assertThat(testBankAccount).isNotNull().isInstanceOf(BankAccount.class);
 
     }
-
 
 
 }
