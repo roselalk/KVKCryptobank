1cb70c7e3c19c9972fb359c44ba53016c9f6d0b5
Roos Lalk <roos.lalk@hva.nl>
2022-09-13 10:25:07 +0200

    Static folder refactored naar Resources en login.js aangepast naar werkend

--- src/main/resources/static/login.js
+++ src/main/resources/static/login.js
@@ -1,6 +1,4 @@
 let button = document.querySelector("#button");
-let emailInput = document.querySelector("#email").value;
-let passwordInput = document.querySelector("#pw").value;
 
 button.addEventListener("click", () => {
     login();
@@ -9,31 +7,30 @@ button.addEventListener("click", () => {
 let myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/json");
 
-let raw = JSON.stringify({
-    "email": `${emailInput}`,
-    "password": `${passwordInput}`
-});
-
 function login() {
+    let emailInput = document.querySelector('#email').value;
+    let passwordInput = document.querySelector('#pw').value;
 
+    let raw = JSON.stringify({
+        "email": emailInput,
+        "password": passwordInput
+    });
 
-    // let data = {email: emailInput, password: passwordInput};
-
-    // console.log(data);
-    // console.log(JSON.stringify(data));
-
-    console.log("pre-fetch")
     let requestOptions = {
         method: 'POST',
         headers: myHeaders,
         body: raw,
         redirect: 'follow'
     };
-    console.log("post-reqOpt")
+    console.log(raw)
     fetch("http://localhost:8080/login", requestOptions)
         .then(response => response.text())
         .then(result => console.log(result))
         .catch(error => console.log('error', error));
+
+
+    console.log("email: " + emailInput)
+    console.log("wachtwoord: " + passwordInput)
 
 
 }
